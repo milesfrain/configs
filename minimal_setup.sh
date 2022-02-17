f0ff8ee833098c63eb7eb2ecc35cddaf50db3a0d
Miles Frain <miles.frain@colorado.edu>
2022-02-17 15:55:46 -0800

    add minimal_setup.sh

---
+++ minimal_setup.sh
@@ -0,0 +1,35 @@
+# minimal setup
+
+set -xe
+
+dir=$(dirname $(realpath $0))
+
+dotfiles=(
+    "$dir"/tmux/.tmux.conf
+    "$dir"/tmux/.reset.tmux.conf
+    "$dir"/zsh/.zshrc
+)
+
+for d in "${dotfiles[@]}"
+do
+  ln -s $d ~/$(basename $d)
+done
+
+./get-tmux-rst.sh
+
+# zsh
+rm -rf ~/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
+
+# fzf
+rm -rf ~/.fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --key-bindings --completion --no-update-rc
+
+# ./vim_setup.sh
+
+echo Run the following command, then log out and in:
+echo chsh -s $(which zsh)
