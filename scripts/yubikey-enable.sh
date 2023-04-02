b6cd49de263cba1dc144df67c644a3897c63a5ea
Miles Frain <miles.frain@colorado.edu>
2023-04-01 23:20:08 -0700

    yubikey echo

--- scripts/yubikey-enable.sh
+++ scripts/yubikey-enable.sh
@@ -4,6 +4,7 @@
 
 YUBIKEY_ID=$(xinput --list | grep Yubico | perl -lane 'print m/id=(\d+)/g')
 
+echo enabling yubikey for 5 seconds
 xinput set-int-prop $YUBIKEY_ID "Device Enabled" 8 1
 sleep 5
 xinput set-int-prop $YUBIKEY_ID "Device Enabled" 8 0
