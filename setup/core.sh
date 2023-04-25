c5b5a29d2740c5cff17f9ce5fbb410cada706d1a
Miles Frain <miles.frain@colorado.edu>
2023-04-24 21:09:32 -0700

    stow p10k

--- setup/core.sh
+++ setup/core.sh
@@ -27,6 +27,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+stow p10k
+
 # fzf
 rm -rf ~/.fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
