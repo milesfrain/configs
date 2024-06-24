56476244872a3f1e952c3515027e76a46a9481dc
Miles Frain <miles.frain@colorado.edu>
2024-06-24 16:29:24 -0700

    Add astrovim rust language pack

--- nvim/.config/nvim/lua/user/plugins/community.lua
+++ nvim/.config/nvim/lua/user/plugins/community.lua
@@ -7,4 +7,5 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.rust" },
 }
