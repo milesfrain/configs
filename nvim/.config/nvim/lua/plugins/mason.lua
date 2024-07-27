376331a39ca66ceb4a5279eb864b62e3d2d49387
Miles Frain <miles.frain@colorado.edu>
2024-07-26 21:13:28 -0700

    Convert to AstroVim V4 template
    https://github.com/AstroNvim/template

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,20 +1,18 @@
--- customize mason plugins
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Mason plugins
+
+---@type LazySpec
 return {
-  {
-    "williamboman/mason.nvim",
-    -- overrides `require("mason").setup(...)`
-    opts = {
-      -- Use system clangd installation, instead of mason's copy
-      -- https://github.com/williamboman/mason.nvim/issues/668#issuecomment-1320859097
-      PATH = "append",
-    },
-  },
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "lua_ls",
+        -- add more arguments for adding more language servers
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -22,14 +20,20 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = {
+        "stylua",
+        -- add more arguments for adding more null-ls sources
+      },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      -- ensure_installed = { "python" },
+      ensure_installed = {
+        "python",
+        -- add more arguments for adding more debuggers
+      },
     },
   },
 }
--- nvim/.config/nvim/lua/user/plugins/treesitter.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    -- Automatically install parsers for opened files
-    -- auto_install = true,
-  },
-}
