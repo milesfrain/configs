7a7f94b0fb6f41c11edf368adad7851be1447495
Miles Frain <miles.frain@colorado.edu>
2024-07-26 19:31:43 -0700

    Revert "Todo - Get C-Tab working in vim"
    This reverts commit 9afc6c7d3c7ab422ad046b9af9b096617b6f483d.

--- nvim/.config/nvim/lua/user/mappings.lua
+++ nvim/.config/nvim/lua/user/mappings.lua
@@ -20,25 +20,11 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    
-    -- Control-Tab to cycle through buffers
-    -- Works in alacritty, but not in standard terminals
-    -- https://old.reddit.com/r/neovim/comments/qemfx5/why_do_ctab_cstab_only_work_in_guis/
-    ["<C-Tab>"] = { ":bnext<cr>", desc = "Next buffer" },
-    ["<C-S-Tab>"] = { ":bprev<cr>", desc = "Previous buffer" },
-    -- Note that this can be improved to work with other buffers (e.g. tree)
-    -- There's also a V3 to V4 migration consideration:
-    -- https://discord.com/channels/939594913560031363/1225452277775732796
-    -- ["<C-Tab>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-    -- ["<C-S-Tab>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-    -- Existing (slow) bindings to cycle through buffers:
-    -- ]b
-    -- [b
-    
-    -- Existing bindings to jump to a buffer:
+    -- Alternatives for tab selection:
     --   <leader>bb
     --   <leader>fb
+    -- ["<C-Tab>"] = { ":bnext<cr>", desc = "Next tab" },
+    -- ["<C-S-Tab>"] = { ":bprev<cr>", desc = "Previous tab" },
   },
   t = {
     -- setting a mapping to false will disable it
