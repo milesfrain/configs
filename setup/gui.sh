0ba48eb9ee5e938bf664f49650bd2c6f6509b823
Miles Frain <miles.frain@colorado.edu>
2024-08-05 14:26:28 -0700

    Comment-out some gui apps and corrections in pop-os

--- setup/gui.sh
+++ setup/gui.sh
@@ -15,20 +15,24 @@ sudo apt install -y \
   xcape \
   xclip
 
+# Do not rely on snap with pop-os
+
 # Cannot merge into single line
-sudo snap install alacritty --classic
-sudo snap install code --classic
-sudo snap install drawio --classic
-sudo snap install emacs --classic
-sudo snap install slack --classic
+#sudo snap install alacritty --classic
+#sudo snap install code --classic
+#sudo snap install drawio --classic
+#sudo snap install emacs --classic
+#sudo snap install slack --classic
 
 stow alacritty
-stow code
+# stow code
 
+# Works by default on pop-os 22.04
 # Working alt-shift-tab
-gsettings reset org.gnome.desktop.input-sources xkb-options
+# gsettings reset org.gnome.desktop.input-sources xkb-options
 
+# Works by default on pop-os 22.04
 # Improve double-click word selection
 # https://unix.stackexchange.com/questions/290544/double-click-selection-in-gnome-terminal
-puuid=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
-gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$puuid/ word-char-exceptions '@ms "-=&#:/.?@+~_%;"'
+# puuid=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
+# gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$puuid/ word-char-exceptions '@ms "-=&#:/.?@+~_%;"'
