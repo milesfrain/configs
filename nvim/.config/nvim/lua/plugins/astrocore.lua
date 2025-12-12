15d0523ceafea3532c7349fd2922ab1d31eb5a17
Miles Frain <miles.frain@colorado.edu>
2025-12-11 17:39:15 -0800

    C-n to copy selected reference to clipboard

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -10,48 +8,48 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- passed to `vim.filetype.add`
-    filetypes = {
-      -- see `:h vim.filetype.add` for usage
-      extension = {
-        foo = "fooscript",
-      },
-      filename = {
-        [".foorc"] = "fooscript",
-      },
-      pattern = {
-        [".*/etc/foo/.*"] = "fooscript",
-      },
-    },
-    -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
-      },
-    },
+    --  -- Configure core features of AstroNvim
+    --  features = {
+    --    large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+    --    autopairs = true, -- enable autopairs at start
+    --    cmp = true, -- enable completion at start
+    --    diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
+    --    highlighturl = true, -- highlight URLs at start
+    --    notifications = true, -- enable notifications at start
+    --  },
+    --  -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+    --  diagnostics = {
+    --    virtual_text = true,
+    --    underline = true,
+    --  },
+    --  -- passed to `vim.filetype.add`
+    --  filetypes = {
+    --    -- see `:h vim.filetype.add` for usage
+    --    extension = {
+    --      foo = "fooscript",
+    --    },
+    --    filename = {
+    --      [".foorc"] = "fooscript",
+    --    },
+    --    pattern = {
+    --      [".*/etc/foo/.*"] = "fooscript",
+    --    },
+    --  },
+    --  -- vim options can be configured here
+    --  options = {
+    --    opt = { -- vim.opt.<key>
+    --      relativenumber = true, -- sets vim.opt.relativenumber
+    --      number = true, -- sets vim.opt.number
+    --      spell = false, -- sets vim.opt.spell
+    --      signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+    --      wrap = false, -- sets vim.opt.wrap
+    --    },
+    --    g = { -- vim.g.<key>
+    --      -- configure global vim variables (vim.g)
+    --      -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+    --      -- This can be found in the `lua/lazy_setup.lua` file
+    --    },
+    --  },
     -- Mappings can be configured through AstroCore as well.
     -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
@@ -59,19 +57,19 @@ return {
       n = {
         -- second key is the lefthand side of the map
 
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        --  -- navigate buffer tabs
+        --  ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        --  ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
+        --  -- mappings seen under group name "Buffer"
+        --  ["<Leader>bd"] = {
+        --    function()
+        --      require("astroui.status.heirline").buffer_picker(
+        --        function(bufnr) require("astrocore.buffer").close(bufnr) end
+        --      )
+        --    end,
+        --    desc = "Close buffer from tabline",
+        --  },
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
@@ -79,6 +77,45 @@ return {
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+
+        -- copy file reference to clipboard
+        ["<C-n>"] = {
+          function()
+            local filepath = vim.fn.expand "%:."
+            local line = vim.fn.line "."
+            local reference = filepath .. ":" .. line
+            vim.fn.setreg("+", reference)
+            vim.notify("Copied: " .. reference, vim.log.levels.INFO)
+          end,
+          desc = "Copy file reference to clipboard",
+        },
+      },
+      v = {
+        -- copy file reference with line range in visual mode
+        ["<C-n>"] = {
+          function()
+            local filepath = vim.fn.expand "%:."
+            -- Get visual selection range
+            local start_line = vim.fn.line "v"
+            local end_line = vim.fn.line "."
+            -- Ensure start is before end
+            if start_line > end_line then
+              start_line, end_line = end_line, start_line
+            end
+            local reference
+            if start_line == end_line then
+              reference = filepath .. ":" .. start_line
+            else
+              reference = filepath .. ":" .. start_line .. "-" .. end_line
+            end
+            vim.fn.setreg("+", reference)
+            vim.notify("Copied: " .. reference, vim.log.levels.INFO)
+            vim.cmd "normal! \27" -- ESC to exit visual mode
+            -- vim.fn.cursor(start_line, 1) -- Jump to start_line
+            -- vim.cmd "normal! ^" -- Jump to first non-blank character
+          end,
+          desc = "Copy file reference with line range to clipboard",
+        },
       },
     },
   },
