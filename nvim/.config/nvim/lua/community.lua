3e3c171053890c1d65db065e3df31e6d9f4685cb
Miles Frain <miles.frain@colorado.edu>
2026-01-07 11:31:36 -0800

    Enable bqf for better reference navigation

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -9,4 +9,5 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.quickfix.nvim-bqf" },
 }
